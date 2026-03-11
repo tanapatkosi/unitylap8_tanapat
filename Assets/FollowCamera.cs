fb80fe0c73a492f431b695b83844346960cb257b
tanapatkosi <tanapat.kosi@bumail.net>
2026-03-11 22:02:58 +0700

    gssg

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+
+{
+
+    [SerializeField] Transform target;
+
+    [SerializeField] float smoothSpeed = 2f;
+
+    [SerializeField] Vector3 offset = new Vector3(0,2,-5);
+
+    void LateUpdate()
+
+    {
+
+        Vector3 desiredPosition = target.position + target.TransformDirection(offset);
+
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        transform.LookAt(target);
+
+    }
+
+}
+ 
